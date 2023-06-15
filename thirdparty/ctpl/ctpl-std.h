511b1d194167df7e420052807420cd54d5dae5d9
Buqian Zheng <zhengbuqian@gmail.com>
2023-06-15 15:10:47 +0000

    Try back pressure

--- thirdparty/ctpl/ctpl-std.h
+++ thirdparty/ctpl/ctpl-std.h
@@ -135,8 +135,8 @@ namespace ctpl {
         thread_pool(int nThreads) {
             this->init();
             this->resize(nThreads);
-            LOG_KNOWHERE_INFO_ << "thread_pool queue limit: " << nThreads;
-            this->q.set_limit(nThreads);
+            LOG_KNOWHERE_INFO_ << "thread_pool queue limit: " << 2 * nThreads;
+            this->q.set_limit(2 * nThreads);
         }
 
         // the destructor waits for all the functions in the queue to be finished
